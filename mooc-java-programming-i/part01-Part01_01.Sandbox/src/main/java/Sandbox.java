86dcae796c800d2d341f21264f4092ec3354b0f2
MattLa <matt.d.laurie@gmail.com>
2021-11-11 20:35:47 -0700

    Two compeleted Exercises

--- mooc-java-programming-i/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ mooc-java-programming-i/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -3,5 +3,10 @@ public class Sandbox {
 
     public static void main(String[] args) {
         // Write your program here
+        /*
+        This is a sample meant to be left empty and used as a test to ensure TMC 
+        is connecting properly with your machine. You should pass all tests by 
+        making absolutely no changes
+        */
     }
 }
--- mooc-java-programming-i/part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
+++ mooc-java-programming-i/part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
@@ -3,5 +3,9 @@ public class AdaLovelace {
 
     public static void main(String[] args) {
         // Write your program here
+        /*
+        This will print the line 'Ada Lovelace' to the console
+        */
+        System.out.println("Ada Lovelace");
     }
 }
